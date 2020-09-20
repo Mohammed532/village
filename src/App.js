882cceefa0b8e2ebfc746ce85e1b4cd95e49b210
Mohammed Akinbayo <35383252+Mohammed532@users.noreply.github.com>
2020-09-19 21:20:40 -0400

    dakj

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from 'Header'
 
 function App() {
   return (
     <div className="body">
+        {Header}
         <h1>Village</h1>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+        return (
+          <div className="body">
+              <h1>Hello</h1>
+          </div>
+        );
+}
+      
+export default Header;
